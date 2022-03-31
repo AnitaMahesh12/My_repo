79aaf9a67b9c00c666ef37b8efac00b90a67470a
root <root@ip-172-31-24-84.ec2.internal>
2022-03-31 04:49:47 +0000

    modified

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 Happiness is learning new things daily
 Happiness is state of mind
 Happiness is meditating
-Happiness is part of life
-Happiness is life
+Happiness
--- test2.sh
+++ test2.sh
@@ -1,2 +1,1 @@
-The only thing that will make you happy, is being happy with who you
-Happiness is life
+The only thing that will make you happy, is being happy with who you are
